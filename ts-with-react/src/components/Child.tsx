fb3fd327f60430afca72e86714e5f4a842521707
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2022-08-14 15:15:14 +0500

    lesson-49

---
+++ ts-with-react/src/components/Child.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Child = () => {
+  return (
+    <div>Children</div>
+  )
+}
+
+export const Child2 = () => {
+  return (
+    <div>Child2</div>
+  )
+}
---
+++ ts-with-react/src/components/Parent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Child, Child2 } from './Child'
+
+const Parent = () => {
+  return (
+    <div> 
+        <Child/> 
+        <Child2/>
+    </div>
+  )
+}
+
+export default Parent
